8f42c706eb9a99f4c5522fbe94d68ee2fc3af676
Raju <rajuganipireddi@gmail.com>
2018-03-21 20:10:52 +0530

    implemented name validation.

--- src/main/java/com/register/CommentsController.java
+++ src/main/java/com/register/CommentsController.java
@@ -1,7 +1,13 @@
 package com.register;
 
 public class CommentsController {
+	String name ="Raju";
 	void method() {
+		if(name != null){
+			System.out.println("name valid");
+		} else {
+			System.out.println("name is invalid");
+		}
 		System.out.println("hello welcome to comments ");
 		
 	}
