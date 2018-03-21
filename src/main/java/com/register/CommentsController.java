bc1bf18b6ba7c211d7f45b26b1d9afb14cfad733
pravalika <pravalika@192.168.0.3>
2018-03-21 20:08:48 +0530

    mobile validation added

--- src/main/java/com/register/CommentsController.java
+++ src/main/java/com/register/CommentsController.java
@@ -1,10 +1,19 @@
 package com.register;
 
 public class CommentsController {
+	String mobile = "89898989";
+
 	void method() {
+		if (mobile != null) {
+			System.out.println("mobile no valid");
+		} else {
+			System.out.println("mobile no not valid");
+		}
+
 		System.out.println("hello welcome to comments ");
-		
+
 	}
+
 	public CommentsController() {
 		System.out.println("default constructor level");
 		System.out.println("zero argument constructor");
