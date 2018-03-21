bcba60d0b17e452fb2db413a3ccb49b53d8fc13e
yash <yash@192.168.0.12>
2018-03-21 08:06:04 +0530

    added validation

--- src/main/java/com/dao/LoginDao.java
+++ src/main/java/com/dao/LoginDao.java
@@ -4,5 +4,11 @@ public class LoginDao {
 
 	public static void main(String[] args) {
 		System.out.println("implemantaion login dao");
+		String name="raju";
+		if(name != null) {
+			System.out.println("Name is not null");
+		} else {
+			System.out.println("name is null");
+		}
 	}
 }
