14ab6012d8ed0a533bdd6bc629fb7d4a74e3d85f
dusavinay <dusavinay@vinay>
2018-03-21 15:08:24 +0530

    dev test completed

--- src/main/java/com/register/CommentsController.java
+++ src/main/java/com/register/CommentsController.java
@@ -3,5 +3,9 @@ package com.register;
 public class CommentsController {
 	void method() {
 		System.out.println("hello welcome to comments ");
+		
+	}
+	public CommentsController() {
+		System.out.println("default constructor level");
 	}
 }
