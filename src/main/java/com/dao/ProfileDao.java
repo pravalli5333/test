ec4ef971c833a6d0c097cb34215e46fe5e83f390
pravalika <pravalika@192.168.0.14>
2018-03-21 08:03:24 +0530

    updated

--- src/main/java/com/dao/ProfileDao.java
+++ src/main/java/com/dao/ProfileDao.java
@@ -3,9 +3,10 @@ package com.dao;
 public class ProfileDao {
 
 	public ProfileDao() {
-	System.out.println("....");
+		System.out.println("....");
 	}
-public void profile(){
-System.out.println("updated");
-}
+
+	public void profile() {
+		System.out.println("updated");
+	}
 }
