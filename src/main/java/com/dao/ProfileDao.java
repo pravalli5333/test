46b2685b2c85db8dd9bf22d3ebe75eaf8d9784ff
pravalika <pravalika@192.168.0.11>
2018-03-22 07:43:49 +0530

    updated

--- src/main/java/com/dao/ProfileDao.java
+++ src/main/java/com/dao/ProfileDao.java
@@ -3,6 +3,13 @@ package com.dao;
 public class ProfileDao {
 
 	public ProfileDao() {
+		String mobile="898989889";
+		if(mobile!=null){
+			System.out.println("valid");
+		}else{
+			System.out.println("not valid");
+		}
+				
 		System.out.println("....");
 	}
 
